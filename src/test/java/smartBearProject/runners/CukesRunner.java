933069ac9335004e9ab4f4d94f9577ec8cedd59e
Hasan YUCEL <hsnnycll@gmail.com>
2023-02-04 16:33:35 +0300

    add new file

---
+++ src/test/java/smartBearProject/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package smartBearProject.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "smartBearProject/stepDefinitions",
+        tags = "@excelReader",
+        dryRun = false
+)
+
+
+public class CukesRunner {
+}
